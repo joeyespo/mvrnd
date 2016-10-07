c223228032c873589afaeb36cabfd4181181e71d
Joe Esposito <joe@joeyespo.com>
2016-10-07 18:16:04 -0400

    Handle counting in the recursive call:
    This prevents multiple "No files to move!" messages from being printed.

--- mvrnd.py
+++ mvrnd.py
@@ -45,7 +45,7 @@ def move_random_file(from_dir, to_dir, collect=None):
         filenames = [f for f in filenames if f not in collected_here]
     if len(filenames) == 0:
         print('-> No files to move!')
-        return
+        return False
 
     file_index = random.randint(0, len(filenames) - 1)
     filename = filenames[file_index]
@@ -56,7 +56,7 @@ def move_random_file(from_dir, to_dir, collect=None):
 
     if collect is not None:
         collect.setdefault(from_dir, []).append(filename)
-        return
+        return True
 
     try:
         os.makedirs(to_dir)
@@ -65,11 +65,14 @@ def move_random_file(from_dir, to_dir, collect=None):
             raise
 
     shutil.move(from_path, to_dir)
+    return True
 
 
-def move_random_files(from_dir, to_dir, collect=None):
+def move_random_files(from_dir, to_dir, count=None, collect=None):
     # Move random non-special files and subdirectories
-    move_random_file(from_dir, to_dir, collect)
+    for i in range(count or 1):
+        if not move_random_file(from_dir, to_dir, collect):
+            break
 
     # Recurse through special directories
     for filename in os.listdir(from_dir):
@@ -91,14 +94,12 @@ def move_random_files(from_dir, to_dir, collect=None):
                 continue
 
         # Repeated recursions
-        count = _extract_attribute(next_from_dir, r'\+', r'[0-9]+')
-        if count:
-            repeat = int(count) - 1
-            for i in range(repeat):
-                move_random_file(next_from_dir, next_to_dir, collect)
+        next_count = _extract_attribute(next_from_dir, r'\+', r'[0-9]+')
+        if next_count:
+            next_count = int(next_count)
 
         # Recurse
-        move_random_files(next_from_dir, next_to_dir, collect)
+        move_random_files(next_from_dir, next_to_dir, next_count, collect)
 
 
 def _extract_attribute(name, delimiter, pattern):
